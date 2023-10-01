b23a89b1baca35b05c1c1c54b67062280db5a1b3
eugeniocachiombo <eugeniocachiombo@gmail.com>
2023-10-01 16:44:51 +0100

    Interface Crianda ainda só em equipa

--- index.php
+++ index.php
@@ -2,46 +2,8 @@
 <a href="views/jogador/">Jogador</a> <br>
 <a href="views/jogo/">Jogo</a> <br>
 
-<?php
-    include 'class/equipa.php';
-    include 'class/jogador.php';
-    include 'class/jogo.php';
-    include 'dao/equipa_dao.php';
-    include 'dao/jogador_dao.php';
-    include 'dao/jogo_dao.php';
-    
-    /* 
-     * 
-     * Sempre que for para instânciar:
-     * Equipa recebe 2 argumentos como parâmetro após ser instanciado, por intermédio do que lhe foi passado ao do construtor
-     * Jogador recebe 5 argumentos como parâmetro após ser instanciado, por intermédio do que lhe foi passado ao do construtor
-     * jogo recebe 7 argumentos como parâmetro após ser instanciado, por intermédio do que lhe foi passado ao do construtor
-     * 
-     */
-    
-        
-   
+<?php 
 
-    /*
-        $equipaDAO = new EquipaDao();
-        $retornoDao = $equipaDAO->RetornoEquipas(26);
+//include "views/jogo/CRUD/jogos_marcados.php"; 
 
-        $equipaA = new Equipa($retornoDao["idEquipa"], $retornoDao["nomeEquipa"]);
-        $equipaB = new Equipa($retornoDao["idEquipa"], $retornoDao["nomeEquipa"]);
-
-        $jogo = new Jogo(4, "Partida Amigável", $equipaA, $equipaB, "10:30", "2024-04-18", false, false);
-        $jogoDAO = new JogoDao();
-
-        //$jogoDAO->Cadastrar($jogo);
-        //$jogoDAO->Actualizar($jogo);
-        //$jogoDAO->Eliminar($id);
-        //$jogoDAO->Buscar();
-        echo "<hr>";
-
-        $retornoMetodos = $jogoDAO->RetornoJogos(6);
-        $Busca = new Jogo($retornoMetodos["idJogo"], $retornoMetodos["temaJogo"], $equipaA, $equipaB, $retornoMetodos["horaJogo"], $retornoMetodos["dataJogo"], $retornoMetodos["estadoJogo"], $retornoMetodos["infoJogo"]);
-        $Busca->MarcarJogo();
-        $Busca->Jogar();
-        $Busca->info();
-    */
 ?>
--- views/equipa/index.php
+++ views/equipa/index.php
@@ -1,17 +1,24 @@
+<?php include '../../views/inc/headHTML.html';?>
+<?php include '../../views/inc/header.html';?>
+
 <title>Formulário de Equipa</title>
 
-<body>
-    <a href="../../">Início</a> <hr>
-    <form action="CRUD/index.php" method="post">
-        <b>Formulário de Equipa</b> <br> <br>
-        <input type="text" name="idEquipa" id="idEquipa" placeholder="Id" style="width: 50px">
-        <input type="text" name="nomeEquipa" id="nomeEquipa" placeholder="Nome"> <br> <br>
-        <input type="submit" value="Cadastrar" name="cadastrar"> 
-        <input type="submit" value="Actualizar" name="actualizar">
-        <input type="submit" value="Eliminar" name="eliminar">
+<div class='container-fluid'>
+    <a href='../../'>Início</a>
+    <hr>
+    <form class="text-center" action='CRUD/index.php' method='post'>
+        <legend><b>Formulário de Equipa <hr></b></legend> <br>
+        <input type='text' name='idEquipa' id='idEquipa' placeholder='Id' style='width: 50px'>
+        <input type='text' name='nomeEquipa' id='nomeEquipa' placeholder='Nome'> <br> <br>
+        <input type='submit' value='Cadastrar' name='cadastrar'>
+        <input type='submit' value='Actualizar' name='actualizar'>
+        <input type='submit' value='Eliminar' name='eliminar'>
     </form>
-    
-    <?php include "CRUD/tabela.php"; ?>
-</body>
 
+    <?php include 'CRUD/tabela.php';
+?>
+</div>
 
+<br>
+<?php include '../../views/inc/footer.html';?>
+<?php include '../../views/inc/footHTML.html';?>
